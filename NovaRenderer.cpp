465520fd58568f75b4486cdb1f6c0fa452bd9b29
Kyran <kyranjdon@gmail.com>
2025-03-11 09:59:02 +1100

    Added camera and perspective grid

--- NovaRenderer.cpp
+++ NovaRenderer.cpp
@@ -52,17 +52,17 @@ int main() {
 
         Gizmos::addTransform(glm::mat4(1));
 
-        vec4 white(1);
+        vec4 red(1,1,0,1);
         vec4 black(0, 0, 0, 1);
 
         for (int i = 0; i < 21; ++i) {
             Gizmos::addLine(vec3(-10 + i, 0, 10),
                 vec3(-10 + i, 0, -10),
-                i == 10 ? white : black);
+                i == 10 ? red : black);
 
             Gizmos::addLine(vec3(10, 0, -10 + i),
                 vec3(-10, 0, -10 + i),
-                i == 10 ? white : black);
+                i == 10 ? red : black);
         }
 
         Gizmos::draw(projection * view);
